814652a7ff397f205e631775d146fbd84a9ad5f9
Svett Ralchev <sralchev@pivotal.io>
2015-12-18 14:39:10 +0000

    Works end to end
    Signed-off-by: Jamie Monserrate <jmonserrate@pivotal.io>

---
+++ create-pipeline.sh
@@ -0,0 +1,3 @@
+#!/bin/bash -ex
+
+curl http://192.168.100.4:8080/api/v1/cli?arch=amd64&platform=linux
---
+++ lib/app.rb
@@ -0,0 +1,18 @@
+require 'sinatra'
+require 'haml'
+
+get '/' do
+  status 200
+  haml :index
+end
+
+get '/fly' do
+  status 200
+  `curl http://192.168.100.4:8080/api/v1/cli?arch=amd64&platform=linux > /tmp/fly; chmod +x /tmp/fly`
+  body `fly`
+end
+
+post '/create-pipeline' do
+  `./reconfigure #{params['repo-name']} #{params['repo-url']}`
+  "Hi #{params['repo-url']}"
+end
